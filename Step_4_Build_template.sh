61bc1250f0c9e76a196290c2a5268dd4ea470d9a
sabrinaselleck <60483160+sabrinaselleck@users.noreply.github.com>
2023-06-19 21:59:18 -0400

    Delete older version of template build script

--- Step_4_Build_template.sh
+++
@@ -1,5 +0,0 @@
-# This bash script builds a template from a directory called 'Inputs' saved within the same directory as this file
-# A template called 'warped_template.nii.gz' and individually transformed scans will be saved in the same directory as the input scans.
-
-cd ./Inputs
-source ~/bin/ants/buildtemplateparallel.sh -n 0 -d 3 -o warped_ c 2 -m 200x200x200 -j 10 -s MSQ -r 1 -t GR *nii.gz
